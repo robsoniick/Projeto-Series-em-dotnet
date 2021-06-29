074099f9805afa3deef1f8fa5d22e82feca0fad8
robsoniick <robson_barbosa@ymail.com>
2021-06-29 13:59:47 -0300

    Firts commit

---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using Dio.Series.Interfaces;
+
+
+namespace Dio.Series
+{
+
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+        public void Atualiza(int id, Serie objeto)
+        {
+            listaSerie[id] = objeto;
+        }
+
+        public void Exclui(int id)
+        {
+            listaSerie[id].Excluir();
+            // implemento envio de e-mail
+        }
+
+        public void Insere(Serie objeto)
+        {
+            listaSerie.Add(objeto);
+        }
+
+        public List<Serie> Lista()
+        {
+            return listaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            return listaSerie[id];
+        }
+    }
+}
